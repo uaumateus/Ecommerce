5506d4b7a765d027ff341150f6c8c441b6895e8b
Mateus Santos <mateus.pereira99@hotmail.com>
2019-09-02 09:45:16 -0300

    COnfigurando rotas

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
 import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      <Home />
+    <div>
+      <Switch>
+        <Route path="/" exact component={Home}/>
+      </Switch>
     </div>
   );
 }
