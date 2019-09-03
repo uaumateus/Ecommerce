8c44240c72405a786478b58c4a6fe0e5bc7ede22
Mateus Santos <mateus.pereira99@hotmail.com>
2019-09-03 10:30:12 -0300

    Cards de informações da tela inicial

---
+++ src/components/CardsBanner/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './style.css';
+
+const CardsBanner = () => {
+    return(
+        <div className="cardsBanner">
+            <div className="cardItem">
+                <article>
+                </article>
+                <p className="Large-Text-Regular">Produtos à pronta entrega com rastreamento</p>
+            </div>
+
+            <div className="cardItem">
+                <article>
+                </article>
+                <p className="Large-Text-Regular">Parcelas facilitadas em até 5 vezes sem juros</p>
+            </div>
+
+            <div className="cardItem">
+                <article>
+                </article>
+                <p className="Large-Text-Regular">Toda semana promoção garantida em todo o site</p>
+            </div>
+        </div>
+    )
+}
+export default CardsBanner;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,17 @@
 import React, {Component} from 'react';
+import './style.css';
+
 import BannerInitial from '../../components/BannerInitial';
+import CardsBanner from '../../components/CardsBanner';
 
 export default class Home extends Component{
     render(){
         return(
             <div>
                 <BannerInitial />
+                <div className="content">
+                    <CardsBanner />
+                </div>
             </div>
         )
     }
