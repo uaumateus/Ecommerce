2900ccd7600d94879212985913f031016b0e8752
Mateus Santos <mateus.pereira99@hotmail.com>
2019-09-07 22:26:35 -0300

    Hover nos cards dos produtos

--- src/components/CardProduct/index.js
+++ src/components/CardProduct/index.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
 import './style.css';
 
+import iconBag from './assets/bag_icon.svg';
+import iconFavorite from './assets/favorite_icon.svg';
+
 export default class CardProduct extends Component{
     render(){
         const { product } = this.props;
         return(
             <div className="cardProduct">
                 <article className="imgProduct">
+                    <article className="backgroundHover">
+                        <button className="button buttonTerceary">Ver Mais</button>
+                        <div className="icons">
+                            <img src={iconFavorite} />
+                            <img src={iconBag} />
+                        </div>
+                    </article>
                     <img src={product.image} />
                 </article>
 
