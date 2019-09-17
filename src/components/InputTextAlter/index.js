9edb25287aad10ca93b7a1c25e595b45e2ee56cc
Mateus Santos <mateus.pereira99@hotmail.com>
2019-09-17 09:30:06 -0300

    input change

--- src/components/InputTextAlter/index.js
+++ src/components/InputTextAlter/index.js
@@ -2,15 +2,43 @@ import React, {Component} from 'react';
 import './style.css';
 
 export default class InputTextAlter extends Component { 
+
+    state = {
+        alter: false,
+        valueInput: this.props.value
+    }
+
+    handleAlter = () => {
+        this.setState({alter: !this.state.alter});
+    }
+
+    handleInput = (e) => {
+        this.setState({valueInput: e.target.value});
+    }
+
     render(){
-        const { title, value } = this.props;
+        const { title } = this.props;
+        let { alter, valueInput } = this.state;
         return(
             <div className="inputTextAlter">
                 <p className="Medium-Text-Bold">{title + ":"}</p>
                 <div className="inputContent">
-                    <input type="text" className="input Medium-Text-Regular" placeholder={value} disabled/>
+                    {alter ?
+                        <input type="text" className="input Medium-Text-Regular" value={valueInput} onChange={this.handleInput}/>
+                    :
+                        <input type="text" className="input Medium-Text-Regular" placeholder={valueInput} disabled />
+                    }
+
                     <div className="optionsInput">
-                        <p className="Medium-Text-Regular">editar</p>
+                        {alter ?
+                        <>
+                            <p className="Medium-Text-Regular" onClick={this.handleAlter}>cancelar</p>
+                            <p className="Medium-Text-Regular" onClick={this.handleAlter}>salvar</p>
+                        </>
+                        :
+                            <p className="Medium-Text-Regular" onClick={this.handleAlter}>editar</p>
+                        }
+                           
                     </div>
                 </div>
             </div>
