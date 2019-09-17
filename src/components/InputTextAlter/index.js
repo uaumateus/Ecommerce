8567dae205bd05d228fc5f880b59838c995639f5
Mateus Santos <mateus.pereira99@hotmail.com>
2019-09-17 10:22:55 -0300

    alterar senha e deletar conta

--- src/components/InputTextAlter/index.js
+++ src/components/InputTextAlter/index.js
@@ -4,29 +4,24 @@ import './style.css';
 export default class InputTextAlter extends Component { 
 
     state = {
-        alter: false,
-        valueInput: this.props.value
+        alter: false
     }
 
     handleAlter = () => {
         this.setState({alter: !this.state.alter});
     }
 
-    handleInput = (e) => {
-        this.setState({valueInput: e.target.value});
-    }
-
     render(){
-        const { title } = this.props;
-        let { alter, valueInput } = this.state;
+        const { title, value, onChange, name } = this.props;
+        let { alter } = this.state;
         return(
             <div className="inputTextAlter">
                 <p className="Medium-Text-Bold">{title + ":"}</p>
                 <div className="inputContent">
                     {alter ?
-                        <input type="text" className="input Medium-Text-Regular" value={valueInput} onChange={this.handleInput}/>
+                        <input type="text" name={name} className="input Medium-Text-Regular" value={value} onChange={onChange}/>
                     :
-                        <input type="text" className="input Medium-Text-Regular" placeholder={valueInput} disabled />
+                        <input type="text" className="input Medium-Text-Regular" placeholder={value} disabled />
                     }
 
                     <div className="optionsInput">
--- src/pages/Account/index.js
+++ src/pages/Account/index.js
@@ -3,22 +3,84 @@ import './style.css';
 
 import BreadCrumb from '../../components/BreadCrumb';
 import InputTextAlter from '../../components/InputTextAlter';
+import InputText from '../../components/InputText';
 
 export default class Account extends Component{
+
+    state = {
+        name: null,
+        adress: null,
+        email: null,
+        user: null,
+        alterPassword: false,
+        deleteAccount: false
+    }
+
+    componentDidMount(){
+        this.setState({
+            name: "Mateus Pereira dos Santos",
+            adress: "Rua José Avilar, 2345, Messejana, Fortaleza - CE",
+            email: "mateus@gmail.com",
+            user: "uaumateus"
+        });
+    }
+
+    onChange = e => {
+        if(e.target.name == "name")
+            this.setState({name: e.target.value});
+        else if(e.target.name == "adress")
+            this.setState({adress: e.target.value});
+        else if(e.target.name == "email")
+            this.setState({email: e.target.value});
+        else if(e.target.name == "user")
+            this.setState({user: e.target.value});
+    }
+
+    handlerAlterPassword = () => {
+        this.setState({alterPassword: !this.state.alterPassword});
+    }
+
+    handlerDeleteAccount = () => {
+        this.setState({deleteAccount: !this.state.deleteAccount});
+    }
+
     render(){
+        let { name, adress, email, user, alterPassword, deleteAccount }  = this.state;
         return(
             <div className="content account">
                 <BreadCrumb actualPage="Conta" />
                 <div className="container">
                     <div>
-                        <InputTextAlter title="Nome" value="Mateus Pereira dos Santos" />
-                        <InputTextAlter title="Endereço" value="Rua José Avilar, 2345, Messejana, Fortaleza - CE" />
-                        <InputTextAlter title="E-mail" value="mateus@gmail.com" />
-                        <InputTextAlter title="Usuário" value="uaumateus" />
+                        <InputTextAlter name="name" title="Nome" value={name} onChange={this.onChange}/>
+                        <InputTextAlter name="adress" title="Endereço" value={adress} onChange={this.onChange}/>
+                        <InputTextAlter name="email" title="E-mail" value={email} onChange={this.onChange}/>
+                        <InputTextAlter name="user" title="Usuário" value={user} onChange={this.onChange}/>
                     </div>
                     <div className="optionsAccount">
-                        <button className="button buttonSecundary">Alterar Senha</button>
-                        <button className="button buttonSecundary">Deletar Conta</button>
+                        {!alterPassword && !deleteAccount &&
+                            <>
+                                <button className="button buttonSecundary" onClick={this.handlerAlterPassword}>Alterar Senha</button>
+                                <button className="button buttonSecundary" onClick={this.handlerDeleteAccount}>Deletar Conta</button>
+                            </>
+                        }
+                        {alterPassword &&
+                            <div className="alterPassword">
+                                <p className="Large-Text-Regular">Alterar Senha</p>
+                                <InputText placeholder="Senha atual" type="password"/>
+                                <InputText placeholder="Nova senha" type="password"/>
+                                <InputText placeholder="Confirme a nova senha" type="password"/>
+                                <button className="button buttonSecundary" onClick={this.handlerAlterPassword}>Cancelar</button>
+                                <button className="button buttonSecundary">Salvar</button>
+                            </div>
+                        }
+                        {deleteAccount &&
+                            <div className="deleteAccount">
+                                <p className="Large-Text-Regular">Digite sua senha para deletar a conta</p>
+                                <InputText placeholder="Senha" type="password"/>
+                                <button className="button buttonSecundary" onClick={this.handlerDeleteAccount}>Cancelar</button>
+                                <button className="button buttonSecundary">Deletar</button>
+                            </div>
+                        }
                     </div>
                 </div>
             </div>
