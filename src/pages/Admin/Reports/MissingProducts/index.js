2062ec7afcebbcc40ccad05251ee805735f05de4
Mateus Santos <mateus.pereira99@hotmail.com>
2019-11-27 10:33:40 -0300

    Relatório de produtos em falta

--- src/pages/Admin/Reports/MissingProducts/index.js
+++ src/pages/Admin/Reports/MissingProducts/index.js
@@ -8,29 +8,32 @@ import TableThreeColumns from '../../../../components/Tables/ThreeColumns';
 class MissingProducts extends Component {
 
     state = {
-        products: [
-            {tab1: "005", tab2: "CamisaMockup", tab3: "R$ 50,00"},
-            {tab1: "006", tab2: "CamisaMockup", tab3: "R$ 120,00"},
-            {tab1: "007", tab2: "CamisaMockup", tab3: "R$ 430,00"},
-            {tab1: "005", tab2: "CamisaMockup", tab3: "R$ 210,00"},
-            {tab1: "005", tab2: "CamisaMockup", tab3: "R$ 130,00"}
-        ],
-        loading: false
+        products: [],
+        // loading: false
     }
 
     componentDidMount = async () => {
-        await api.get('/admin/auth').then(resp => {
-            if(!resp.data.result){
-                this.props.history.push('/');
+        // await api.get('/admin/auth').then(resp => {
+        //     if(!resp.data.result){
+        //         this.props.history.push('/');
+        //     }
+        //     else this.setState({loading: true});
+        // }).catch(error => {
+        //     this.props.history.push('/');
+        // })
+        await api.get('/admin/products-outstock').then(resp => {
+            let array = [];
+            for(var i = 0; i < resp.data.length; i++){
+                array.push({tab1: resp.data[i].id, tab2: resp.data[i].name, tab3: "R$ "+resp.data[i].price});
             }
-            else this.setState({loading: true});
-        }).catch(error => {
-            this.props.history.push('/');
+            this.setState({products: array});
+        }).catch(err => {
+            console.log(err)
         })
     }
 
     render(){
-        if (!this.state.loading) return null;
+        // if (!this.state.loading) return null;
         return(
             <div className="content">
                 <BreadCrumb previousPage="Relatórios" actualPage="Produtos em Falta"/>
